9e73998d25b7101edcc62d11718cd8ea1751a252
DENISA-MARIA FILIPAȘ <denisa.filipas@stud.ubbcluj.ro>
2022-10-16 22:47:25 +0300

    Tema 1 - Controllers

---
+++ Controllers/WelcomeController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Filipas_Denisa_Lab1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Bine ai venit!");
+        }
+
+        public string Salut()
+        {
+            return "Salutare!";
+        }
+
+        public IActionResult Actual(string nume, int ani)
+        {
+            return Content("Salutare " +nume +". Ai varsta de "+ani +" ani.");
+        }
+    }
+}
