c5cd8a0eadcfb502e82c1d5042d154aa717068c3
Denisa <Denisa@DESKTOP-6RU2R46>
2022-10-16 21:58:18 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Filipas_Denisa_Lab1.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
